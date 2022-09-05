a9b6d9c288dc98178a678663772ba65282a3b7dd
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-05 19:41:25 +0300

    task_027

---
+++ task_027/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write(number + " -> " + sumNumber(number));
+
+int sumNumber(int number)
+{
+string number1 = Convert.ToString(Math.Abs(number));
+int [] array = new int[number1.Length];
+int Length = number1.Length;
+for ( int index = 0; index < Length; index++ )
+{
+array[index] = Convert.ToInt32(Convert.ToString(number1[index]));
+}
+if(number < 0)
+{
+   array[0]=Convert.ToInt32(Convert.ToString(number1[0]))*-1; 
+}
+int sum = 0;
+for(int index = 0; index < Length; index++ )
+{
+sum += array[index];
+}
+return sum;
+}
