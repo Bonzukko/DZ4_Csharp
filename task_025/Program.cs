d0af8b622a398810922f43e26247a868c19ea977
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-05 19:41:13 +0300

    task_025

---
+++ task_025/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+while(numberB < 1)
+{
+    Console.Write("Введите число B: ");
+   numberB = Convert.ToInt32(Console.ReadLine());
+ }
+int result = Pow(numberA, numberB);
+Console.Clear();
+Console.Write( numberA + ", " + numberB + " -> " + result );
+
+int Pow(int A, int B)
+{
+    int result = A;
+for(int index = 1; index < B; index++)
+{
+     result*=A;
+}
+return result;
+}
