1f02cd7ea5a80dff699c17935d833d79f19b7677
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-05 19:41:36 +0300

    task_029

---
+++ task_029/Program.cs
@@ -0,0 +1,11 @@
+﻿void array(int size)
+{
+int [] array = new int[size];
+for(int index = 0; index < size; index++)
+{
+    array[index]= new Random().Next(0, 100);
+    Console.Write(array[index] + ", ");
+}
+}
+Console.Clear();
+array(8);
